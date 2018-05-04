5462ea4db391a9129e542e51bcb7834220173fe8
Mitch Bonner <mitchell.bonner@gmail.com>
2018-05-04 11:22:01 -0400

    Uploading team results to google docs

--- docs.py
+++ docs.py
@@ -7,6 +7,7 @@ from os.path import expanduser
 
 FILE_ID = '1p_wGW4VdSSHqaUFmxtlRSFnZ0-YCVwwa014ncVXNPfk'
 FULL_FILE_ID = '1IabFOGN4rLEBEcLBqBmypouP7buTotxaG2cjku-eJ38'
+TEAM_FILE_ID = '1ganoHOif2wmrK5jCkwiFwWFH97O9J3ENODYcF6e2LEc'
 
 # TODO: Change placeholder below to generate authentication credentials. See
 # https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
@@ -42,6 +43,7 @@ updatedFile = service.files().update(
 
 sheetsService = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
 
+# INTERNAL VOTING HISTORY
 sheet_metadata = sheetsService.spreadsheets().get(spreadsheetId=FILE_ID).execute()
 sheets = sheet_metadata.get('sheets', '')
 title = sheets[0].get("properties", {}).get("title", "/r/FloridaGatorsPREMIUM Counterintelligence")
@@ -65,6 +67,8 @@ reqs = {'requests': [
 res = sheetsService.spreadsheets().batchUpdate(
         spreadsheetId=FILE_ID, body=reqs).execute()
 
+# FULL VOTING HISTORY
+
 currentFile = service.files().get(fileId=FULL_FILE_ID).execute()
 
 file_metadata = {
@@ -80,8 +84,6 @@ updatedFile = service.files().update(
         fileId=FULL_FILE_ID,
         body=file_metadata,
         media_body=media).execute()
-
-sheetsService = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
 
 sheet_metadata = sheetsService.spreadsheets().get(spreadsheetId=FULL_FILE_ID).execute()
 sheets = sheet_metadata.get('sheets', '')
@@ -106,5 +108,43 @@ reqs = {'requests': [
 res = sheetsService.spreadsheets().batchUpdate(
         spreadsheetId=FULL_FILE_ID, body=reqs).execute()
 
-# TODO: Change code below to process the `response` dict:
-print res.get('id')
+# FULL TEAM HISTORY
+currentFile = service.files().get(fileId=TEAM_FILE_ID).execute()
+
+file_metadata = {
+    'name': 'CFB Risk Full Team History',
+    'mimeType': 'application/vnd.google-apps.spreadsheet'
+}
+
+media = MediaFileUpload('resultsTeamFull.csv',
+        mimetype='text/csv',
+        resumable=True)
+
+updatedFile = service.files().update(
+        fileId=TEAM_FILE_ID,
+        body=file_metadata,
+        media_body=media).execute()
+
+sheet_metadata = sheetsService.spreadsheets().get(spreadsheetId=TEAM_FILE_ID).execute()
+sheets = sheet_metadata.get('sheets', '')
+title = sheets[0].get("properties", {}).get("title", "CFB Risk Full Voting History")
+sheetId = sheets[0].get("properties", {}).get("sheetId", 0)
+print sheetId
+
+reqs = {'requests': [
+    # frozen row 1
+    {'updateSheetProperties': {
+        'properties': { 'sheetId' : sheetId, 'gridProperties': {'frozenRowCount': 1}},
+        'fields': 'gridProperties.frozenRowCount',
+    }},
+    # embolden row 1
+    {'repeatCell': {
+        'range': {'sheetId' : sheetId, 'endRowIndex': 1},
+        'cell': {'userEnteredFormat': {'textFormat': {'bold': True}}},
+        'fields': 'userEnteredFormat.textFormat.bold',
+    }}
+]}
+
+res = sheetsService.spreadsheets().batchUpdate(
+        spreadsheetId=TEAM_FILE_ID, body=reqs).execute()
+
